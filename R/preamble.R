d09a38a38f07b64f40c8c0cd34cd384897033a8c
jpfrench81 <joshua.french@gmail.com>
2016-06-02 07:44:55 -0600

    v 0.1.0 Initial commit.  First stable version.

---
+++ R/preamble.R
@@ -0,0 +1,8 @@
+.onAttach <- function(...) {
+    date <- date()
+    x <- regexpr("[0-9]{4}", date)
+    yr <- substr(date, x[1], x[1] + attr(x, "match.length") - 1)
+	greet <- paste("# This package was created for research supported by NSF Grant DMS-1463642 and NIH Grant R01 CA157528")
+	packageStartupMessage(greet)
+}
+
