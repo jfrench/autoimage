fae4aa30aec2a0bef5085a85600f2e102f707fa7
Joshua French <jfrench@JOSHUAs-MBP.local.tld>
2016-12-08 16:07:29 -0700

    v 0.3.7 Add rotate function to rotate coordinates.  And associated tests.

---
+++ R/rotate.R
@@ -0,0 +1,50 @@
+#' Rotate coordinates
+#'
+#' \code{rotate} rotates coordinates angle theta around a pivot point.
+#' 
+#' @param coords A 2-column matrix with the coordinates to be rotated.
+#' @param theta The angle (in radians) to rotate the coordinates.
+#' @param pivot The pivot point around which the coordinates are rotated.  
+#' Default is c(0, 0), i.e., the origin.
+#' @export
+#' @examples 
+#' # coordinates to rotate
+#' coords <- matrix(rnorm(20), ncol = 2)
+#' # rotate coordinates pi/6 radians around the original
+#' rcoords <- rotate(coords, pi/6)
+#' #compare original coordinates to rotated coordinates
+#' par(mfrow = c(1, 2))
+#' plot(coords)
+#' plot(rcoords)
+rotate <- function(coords, theta, pivot = c(0, 0)){
+  arg.check.rotate(coords, theta, pivot)
+  # rotation matrix
+  rmat <- matrix(c(cos(theta), -sin(theta), sin(theta), cos(theta)), byrow = TRUE, ncol = 2)
+  # rotate coordinates around pivot
+  t(pivot + rmat %*% (t(coords) - pivot))
+}
+
+arg.check.rotate <- function(coords, theta, pivot){
+  if(!is.matrix(coords)){
+    stop("coords must be a matrix")
+  }
+  if(ncol(coords) != 2){
+    stop("coords must have two columns")
+  }
+  if(!is.numeric(coords)){
+    stop("coords must have numeric values")
+  }
+  if(length(theta) != 1){
+    stop("theta must be a single numeric value")
+  }
+  if(!is.numeric(theta)){
+    stop("theta must be a single numeric value")
+  }
+  if(length(pivot) != 2){
+    stop("pivot must be a numeric vector of length 2")
+  }
+  if(!is.numeric(pivot)){
+    stop("pivot must be a numeric vector of length 2")
+  }
+}
+
---
+++ tests/testthat/test-rotate.R
@@ -0,0 +1,28 @@
+# number of points to rotate
+n <- 20
+# random angle rotations
+theta1 <- runif(1, 0, 2*pi)
+theta2 <- runif(1, 0, 2*pi)
+
+# random point to pivot around
+pivot <- rnorm(2)
+
+# create coords and ppp object
+coords <- matrix(rnorm(2*n), nrow = n)
+ppcoords <- spatstat::ppp(coords[,1], coords[,2], 
+                          window = spatstat::owin(range(coords[,1]), range(coords[,2])))
+
+# rotation of theta1 around origin (0, 0)
+rcoords <- rotate(coords, theta1)
+# rotation of theta2 around random pivot point
+rcoords2 <- rotate(coords, theta2, pivot)
+
+# same rotations using spatstat package
+rppcoords = spatstat::rotate(ppcoords, theta1)
+rppcoords2 = spatstat::rotate(ppcoords, theta2, centre = pivot)
+
+# make sure these are equal
+test_that("autoimage::rotate matches spatstat::rotate", {
+  expect_true(all.equal(rcoords, cbind(rppcoords$x, rppcoords$y)))
+  expect_true(all.equal(rcoords2, cbind(rppcoords2$x, rppcoords2$y)))
+})
