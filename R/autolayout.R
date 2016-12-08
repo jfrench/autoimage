5a911daee3040e51cf25dee4644d4f448892a639
Joshua French <jfrench@JOSHUAs-MacBook-Pro.local>
2016-12-08 11:18:06 -0700

    v 0.3.5 Autolayout correct.  Trying to get a clean commit.

--- R/autolayout.R
+++ R/autolayout.R
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #' Divide device into rows and columns
 #'
 #' \code{autolayout} divides the current device into equal-sized rows and equal-sized 
@@ -87,47 +86,10 @@ autolayout <- function(size, legend = FALSE, horizontal = TRUE, common.legend = 
           crow <- (i-1)*2 + 1
           mat[crow, ] <-  (i - 1)*nc*2 + seq_len(nc)*2 - 1
           mat[crow + 1, ] <- (i - 1)*nc*2 + seq_len(nc)*2
-=======
-autolayout <- function(size, legend = FALSE, common.legend = TRUE, outer = FALSE, show = FALSE){
-  arg.check.autolayout(size, legend, common.legend, outer, show)
-  # number of rows and columns desired
-  nr = size[1]
-  nc = size[2]
-  # choose layout depending on whether a legend is required, and if so,
-  # whether the legend should be horizontal or vertical and whether the legend
-  # is common.  Note that the layout is chosen so that the legends are plotted first
-  # then the images, so that lines and points can be added to the image
-  # afterward.
-  if(!legend){ # setup if there is no legend
-    mat = matrix(seq_len(ng), nrow = nr, byrow = TRUE)
-    lheight = rep(1, nr) # common height for all panes
-    lwidth = rep(1, nc) # common width for all panes
-  }else{ # setup if there should be a legend
-    if(common.legend){ # setup the legend is common
-      mat = matrix(seq_len(ng), nrow = nr, byrow = TRUE)
-      # ni = ng + 1
-      if(horizontal){
-        mat = rbind(mat + 1, matrix(1, ncol = nc)) # make sure legend is in first position
-        lheight = c(rep(mratio, nr), 1)
-        lwidth = c(rep(1, nc))
-      }else{
-        mat = cbind(mat + 1, matrix(1, nrow = nr)) # make sure legend is in first position
-        lheight = rep(1, nr)
-        lwidth = c(rep(mratio, nc), 1)
-      }
-    }else{ # setup if the legend is not common
-      if(horizontal){ # horizontal legend
-        mat = matrix(0, nrow = 2 * nr, ncol = nc)
-        for(i in seq_len(nr)){
-          crow = (i-1)*2 + 1
-          mat[crow, ] = (i - 1)*nc*2 + seq_len(nc)*2 # (i - 1)*nc*2 + seq_len(nc)*2 - 1
-          mat[crow + 1, ] = (i - 1)*nc*2 + seq_len(nc)*2 - 1 # (i - 1)*nc*2 + seq_len(nc)*2
->>>>>>> 9dd97fb074c91aaac03a524deddc815b24883f99
         }
         lheight = c(rep(c(mratio, 1), nr))
         lwidth = c(rep(1, nc))
       }else{ # vertical legend
-<<<<<<< HEAD
         mat <- matrix(1:(2*ng), nrow = nr, ncol = 2*nc, byrow = TRUE)
         lheight <- rep(1, nr)
         lwidth <- c(rep(c(mratio, 1), nc))
@@ -187,19 +149,6 @@ autolayout <- function(size, legend = FALSE, common.legend = TRUE, outer = FALSE
 }
 
 arg.check.autolayout <- function(size, legend, horizontal, common.legend, outer, show, mratio){
-=======
-        mat = matrix(1:(2*ng), nrow = nr, ncol = 2*nc, byrow = TRUE) + matrix(rep(c(1, -1), length = ng), nrow = nr, ncol = 2*nc, byrow = TRUE) # the second adjustment is to plot the legend before the image
-        lheight = rep(1, nr)
-        lwidth = c(rep(c(mratio, 1), nc))
-      }
-    }
-  }
-  if(show){
-    
-  }
-}
-
-arg.check.autolayout <- function(size, legend, common.legend, outer, show){
   if(length(size) != 2){
     stop("size should be a vector of length 2")
   }
--- tests/test-all.R
+++ tests/test-all.R
@@ -1,5 +1,2 @@
-<<<<<<< HEAD
 library(testthat)
-=======
->>>>>>> 9dd97fb074c91aaac03a524deddc815b24883f99
 test_check("autoimage")
