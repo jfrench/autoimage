da48cc4fa7da14535abe4be8ef84d2c9062497cc
jpfrench81 <joshua.french@gmail.com>
2020-07-17 12:06:32 -0600

    Fix reference issue to akima::interp function.

--- R/inarccap.R
+++ R/inarccap.R
@@ -2,10 +2,10 @@
 #' @aliases ilon ilat itasmax
 #' @title Interpolated maximum daily surface air temperatures on a regular grid.
 #' @description These data are the \code{narccap} data interpolated onto a regular 
-#' 140\eqn{\times}115 grid using the \code{\link[akima]{interp}} function.
+#' 140\eqn{\times}115 grid using the \code{akima::interp} function.
 #' @docType data
 #' @usage data(inarccap)
-#' @seealso \code{\link[autoimage]{narccap}}, \code{\link[akima]{interp}}
+#' @seealso \code{\link[autoimage]{narccap}}, \code{akima::interp}
 #' @format Contains: \describe{ 
 #' \item{ilon}{A vector of of longitude coordinates.} 
 #' \item{ilat}{A vector of latitude coordinates.} 
